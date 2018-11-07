3deaf1cc3a754b51411e93582774e1dc74d4ca37
B <brandontolor@yahoo.com>
2018-11-06 19:10:01 -0600

    This is the initial commit.

---
+++ src/Exercise_1/Lab9_Ex1.java
@@ -0,0 +1,12 @@
+
+package Exercise_1;
+
+
+public class Lab9_Ex1 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("This exercise was started on 11/6/2018");
+    }
+    
+}
