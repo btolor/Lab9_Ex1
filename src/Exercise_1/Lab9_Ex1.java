6d03f4b7fb0d6d71e87a0d60fb28dc6eeb8b7c89
B <brandontolor@yahoo.com>
2018-11-06 20:15:45 -0600

    I added a user input method and started a recursion method called stars, to print out a specific patter to the console...

--- src/Exercise_1/Lab9_Ex1.java
+++ src/Exercise_1/Lab9_Ex1.java
@@ -1,12 +1,43 @@
 
 package Exercise_1;
 
+import java.util.Scanner;
 
 public class Lab9_Ex1 {
 
     
     public static void main(String[] args) {
-        System.out.println("This exercise was started on 11/6/2018");
+        int lines = lines();
+        stars(lines);
     }
-    
+    //Class scanner object...
+    static Scanner scan = new Scanner(System.in);
+    //User input method...
+    public static int lines(){
+        int x;
+        System.out.print("Enter the number lines: ");
+        while(!scan.hasNextInt()){
+            if (scan.nextInt()<= 0){
+                System.out.println();
+                System.out.print("Please enter a positive integer: ");
+            }            
+        }
+        x =scan.nextInt();
+        return x;
+    }
+
+    //This is a recursion method...
+    public static void stars(int a){
+        int c = a;
+        if (c > 1) { 
+            for(int b =a; b >= 1; b--){
+                System.out.print("*");
+            }
+            System.out.println();
+            c--;
+            stars(c);
+        }
+        
+        
+    }
 }
