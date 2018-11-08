946cb97b606d071fe90987f0d3deaed69954a3bc
B <brandontolor@yahoo.com>
2018-11-08 00:51:18 -0600

    I finished the recursive method to print the stars pattern and added that recursive method to the change user input method...

--- src/Exercise_1/Lab9_Ex1.java
+++ src/Exercise_1/Lab9_Ex1.java
@@ -7,37 +7,35 @@ public class Lab9_Ex1 {
 
     
     public static void main(String[] args) {
-        int lines = lines();
-        stars(lines);
+        lines();
+        
     }
     //Class scanner object...
     static Scanner scan = new Scanner(System.in);
     //User input method...
-    public static int lines(){
+    public static void lines(){
         int x;
         System.out.print("Enter the number lines: ");
-        while(!scan.hasNextInt()){
-            if (scan.nextInt()<= 0){
-                System.out.println();
-                System.out.print("Please enter a positive integer: ");
-            }            
-        }
         x =scan.nextInt();
-        return x;
+        while(x < 1){         
+            System.out.print("Please enter a positive integer: ");
+            x = scan.nextInt();
+        }      
+        stars(x);
     }
 
-    //This is a recursion method...
+    //This is a recursion method to print out the star pattern...
     public static void stars(int a){
-        int c = a;
-        if (c > 1) { 
-            for(int b =a; b >= 1; b--){
+        if (a > 0){
+            for (int i = 0; i < a; i++){
                 System.out.print("*");
             }
-            System.out.println();
-            c--;
-            stars(c);
+                System.out.println();
+            stars (a - 1);
+            for (int i = a; i > 0; i--){
+                System.out.print("*");
+            }
+                System.out.println();
         }
-        
-        
     }
 }
