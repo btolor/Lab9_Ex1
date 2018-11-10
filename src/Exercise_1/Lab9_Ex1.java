12603f3960707ba0e35495053b5dd7acc8e795ca
B <brandontolor@yahoo.com>
2018-11-10 02:26:41 -0600

    I rearranged the exception handling method to the lines user input method and added a comment to the user input method...

--- src/Exercise_1/Lab9_Ex1.java
+++ src/Exercise_1/Lab9_Ex1.java
@@ -5,27 +5,60 @@ import java.util.Scanner;
 
 public class Lab9_Ex1 {
 
-    
     public static void main(String[] args) {
-        lines();
-        
+        lines();        
     }
+    
     //Class scanner object...
     static Scanner scan = new Scanner(System.in);
     //User input method...
+    /**
+    * The lines method is used to get input from the 
+    * user about how many lines of stars to print out
+    * to the console. The method uses a while loop that
+    * loops while an int variable is less than or eqaul
+    * to 0. Within that while loop there is a print
+    * statement that prompts the user for input. That 
+    * input is stored into a string variable and a try
+    * catch method is executed to try to parse the string 
+    * into an integer. If the string variable can be 
+    * parsed into an integer an if statement is executed
+    * to check the value of the integer. If the integer
+    * value is less than or equal to 0 a print statement
+    * is printed to the console else the while loop is 
+    * exited because the integer value is greater than 0.
+    * If the string variable value can't be parsed into 
+    * an integer a print statement is printed to the 
+    * console. Once the while loop is exited an if/else 
+    * statement checks the value of the int variable. If
+    * the value of the int variable is greater 0 the 
+    * stars recursive method is called passing the int 
+    * variable in as its parameter, else the lines 
+    * method is called once again.
+    */
     public static void lines(){
         String lines;
         int x = 0;
-        System.out.print("Enter the number lines: ");
-        lines = scan.nextLine();
-        try {
-            x = Integer.parseInt(lines);
-        } catch (Exception e) {
-            System.out.println("Please enter a valid number!\n");
+        while (x <= 0) {
+            System.out.print("Enter the number lines: ");
+            lines = scan.nextLine();
+            System.out.println();
+            try {
+                x = Integer.parseInt(lines);
+                if( x<= 0){
+                System.out.println("Enter a 'POSITIVE' number!\n");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid entry! Enter a number greater than 0!\n");
+            }
+        }
+        if (x > 0) {
+            System.out.println();
+            stars(x);
+        }else{
+            System.out.println();
             lines();
         }
-   
-        stars(x);
     }
 
     //This is a recursion method to print out the star pattern...
